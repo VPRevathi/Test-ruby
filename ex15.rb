e1cd3d05193f3754d79efd279a102d2ddc7aaef1
VPRevathi <vprevathi@ymail.com>
2018-08-06 19:09:35 +0530

    15th file

---
+++ ex15.rb
@@ -0,0 +1,6 @@
+person1, person2, person3, person4 = ARGV
+puts "There are four peoples:"
+puts "person1 : #{person1}"
+puts "person2 : #{person2}"
+puts "person3 : #{person3}"
+puts "person4 : #{person4}"
