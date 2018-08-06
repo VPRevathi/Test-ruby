920f1b3fc4f652c251c0b0b7a53e9cf94f3bd325
VPRevathi <vprevathi@ymail.com>
2018-08-06 11:30:09 +0530

    second file

---
+++ ex2.rb
@@ -0,0 +1,19 @@
+puts "I will now count types of books in my shelf:"
+
+puts "Fiction #{25 + 30 / 6}"
+puts "Non Fiction #{100 - 25 * 3 % 4}"
+
+puts "Now I will count total number of books"
+
+puts 32 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 5 + 2? #{5 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
