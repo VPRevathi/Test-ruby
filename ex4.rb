3a3e3750bde70dd3f2d8ad4d3da33ea4b31fa3db
VPRevathi <vprevathi@ymail.com>
2018-08-06 11:33:55 +0530

    fourth file

---
+++ ex4.rb
@@ -0,0 +1,14 @@
+my_name = 'Sita'
+my_age = 20
+my_height = 5.2
+my_weight = 48
+my_eyes = 'Brownish'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about my friend #{my_name}."
+puts "She is #{my_height} inches tall."
+puts "She is #{my_weight} kilogram heavy."
+puts "She got #{my_eyes} eyes and #{my_hair} hair."
+puts "Her teeth are usually #{my_teeth}"
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
